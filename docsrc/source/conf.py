06d9e68574be4f8c5021bf466b6d09cc0251b3ee
Nick DeRobertis <nick@claimfound.com>
2019-08-17 10:57:45 -0400

    Add setup for documenting class __init__ methods

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -189,4 +189,12 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
 
