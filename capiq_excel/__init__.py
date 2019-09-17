007ed46fba61d6bc5d2681865c0ab6f606106956
Nick DeRobertis <whoopnip@gmail.com>
2019-09-17 18:08:47 -0400

    Add docstring for main package

--- capiq_excel/__init__.py
+++ capiq_excel/__init__.py
@@ -1,3 +1,7 @@
+"""
+A tool to drive Excel using the Capital IQ plugin to download Capital IQ data. Useful for downloading
+large data sets from Capital IQ.
+"""
 from capiq_excel.main import (
     download_data,
     download_data_for_capiq_ids
